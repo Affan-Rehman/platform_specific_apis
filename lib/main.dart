6ee9092ff039c8f2f2fee2863a5aaf81b4901d7d
Affan Rehman <112381046+Affan-Rehman@users.noreply.github.com>
2024-01-14 21:09:47 +0500

    create main pager

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,60 @@
-import 'package:flutter/material.dart';
+import 'dart:async';
 
-void main() {
-  runApp(const MainApp());
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class PlatformChannel extends StatefulWidget {
+  const PlatformChannel({super.key});
+
+  @override
+  State<PlatformChannel> createState() => _PlatformChannelState();
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class _PlatformChannelState extends State<PlatformChannel> {
+  static const MethodChannel platform = MethodChannel('practice/battery');
+
+// Get battery level.
+  String _batteryLevel = 'Unknown battery level.';
+
+  Future<void> _getBatteryLevel() async {
+    String batteryLevel;
+    try {
+      final result = await platform.invokeMethod<int>('getBatteryLevel');
+      batteryLevel = 'Battery level at $result % .';
+    } on PlatformException catch (e) {
+      batteryLevel = "Failed to get battery level: '${e.message}'.";
+    }
+
+    setState(() {
+      _batteryLevel = batteryLevel;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
+    return Material(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: <Widget>[
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(_batteryLevel, key: const Key('Battery level label')),
+              Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: ElevatedButton(
+                  onPressed: _getBatteryLevel,
+                  child: const Text('Refresh'),
+                ),
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
+}
+
+void main() {
+  runApp(const MaterialApp(home: PlatformChannel()));
 }
